ae3f8103a6fb2a85d0ba5cf12639ad41089483c7
Steven Fehr <shfehr@gmail.com>
2023-09-01 15:31:19 -0500

    --object key method got me on the card tasks

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,35 @@
+import axios from "axios";
+
 const Card = (article) => {
+
+  const fullCard = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imageContainer = document.createElement('div');
+  const image = document.createElement('img');
+  const byAuthor = document.createElement('span')
+
+  fullCard.appendChild(headline);
+  fullCard.appendChild(author);
+  author.appendChild(imageContainer)
+  imageContainer.appendChild(image)
+  author.appendChild(byAuthor)
+
+  fullCard.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imageContainer.classList.add('img-container');
+
+  headline.textContent = article.headline
+  byAuthor.textContent = article.authorName
+  image.src = article.authorPhoto
+
+  fullCard.addEventListener('click', () => {
+    console.log(article.headline)
+  })
+
+  return fullCard
+
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -17,9 +48,42 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+
+
 }
 
+
 const cardAppender = (selector) => {
+  axios.get('http://localhost:5001/api/articles') 
+  .then(resp => {
+      // console.log(resp.data.articles.javascript[0])
+      // for (let topic in resp.data.articles) {
+        // console.log(resp.data.articles)
+        for (let i = 0; i < Object.keys(resp.data.articles).length; i++) {
+
+          // console.log(resp.data.articles[Object.keys(resp.data.articles)[i]])
+            resp.data.articles[Object.keys(resp.data.articles)[i]].forEach(article => {
+            document.querySelector(selector).append(Card(article))
+            // });
+        })
+    }})
+
+       
+        // topic.forEach(article => {
+        // document.querySelector(selector).append(Card(article))
+        //  });
+      //}
+      // resp.data.articles.forEach(topic => {
+      //    topic.forEach(article => {
+      //    document.querySelector(selector).append(Card(article))
+      //    });
+//
+//       document.querySelector(selector)
+//       const cardFinal = Card(topic)
+//       location.appendChild(cardFinal)
+     
+
+  .catch(err => console.error(err))
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
-axios.get('https://localhost:5001/api/topics') 
- .then(resp => {
-  console.log(resp);
- })
- .catch(err => console.error(err))
-
 
 const Tabs = (topics) => {
-
+// console.log(topics)
   const topicsList = document.createElement('div');
+  topicsList.classList.add('topics')
   
   topics.forEach(topic => {
     const tab = document.createElement('div');
@@ -17,9 +12,6 @@ const Tabs = (topics) => {
     tab.textContent = topic
     topicsList.appendChild(tab);
   })
-  
-
-  console.log(topicsList)
   // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
@@ -33,19 +25,41 @@ const Tabs = (topics) => {
   //   <div class="tab">bootstrap</div>
   //   <div class="tab">technology</div>
   // </div>
-  //
+  //  const newHeader = Header("Chicago", "July", "21")
+  
   return topicsList
 }
 
 Tabs(['javascript', 'bootstrap', 'technology'])
 
 const tabsAppender = (selector) => {
-  axios.get('https://localhost:5001/api/topics')
 
-  const newTab = Tabs(['javascript', 'bootstrap', 'technology'])
-  const location = document.querySelector(selector)
+  axios.get('http://localhost:5001/api/topics') 
+   .then(resp => {
+      //  console.log(resp.data.topics)
+       const topics = resp.data.topics
+       const finalTab = Tabs(topics)
+       const tabLocation = document.querySelector(selector)
+       tabLocation.appendChild(finalTab)
+      //  for (let i = 0; i < topics.length; i++) {
+      //    const tabLocation = document.querySelector(selector)
+      //    const finalTab = Tabs(topics[i])
+      //    tabLocation.appendChild(finalTab)
+       //}
 
-  location.appendChild(newTab)
+    // resp.data.topics.forEach(topic => {
+    //   const tabLocation = document.querySelector(selector)
+    //   const finalTab = Tabs(topic)
+    //   tabLocation.appendChild(finalTab)
+    
+    // })
+ })
+   .catch(err => console.error(err))
+
+  // const newTab = Tabs(resp)
+  // const location = document.querySelector(selector)
+
+  // location.appendChild(newTab)
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
@@ -53,6 +67,6 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
+ }
 
 export { Tabs, tabsAppender }
